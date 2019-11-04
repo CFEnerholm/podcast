c4ea4d5a4f3ebfe958bd37a7ccdc8ac9d18c0813
matsnilsson87 <matsnilsson87@gmail.com>
2019-11-04 10:25:46 +0100

    MATS RULE THE WORLD AND RUDDE DANCING FOR HIM!!!

---
+++ Validation/ValidationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+namespace Validation
+{
+    public class ValidationService
+    {
+        public ValidationService()
+        {
+        }
+
+
+
+
+        public void MessageDialog(string message)
+        {
+
+            MessageDialog md = new MessageDialog(null, DialogFlags.DestroyWithParent,
+                                                  MessageType.Error, ButtonsType.Ok, message);
+            md.Title = "Felmedelande";
+            md.Run();
+            md.Destroy();
+        }
+
+    }
+}
